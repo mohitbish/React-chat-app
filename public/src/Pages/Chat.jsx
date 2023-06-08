02ad87e7faa5181918953579c5c31f34f20a6a09
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-09 02:46:35 +1000

    css fixed

--- public/src/Pages/Chat.jsx
+++ public/src/Pages/Chat.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {allUsersRoute} from "../Utils/APIRoutes";
-import "../Styles/Chat.css"
+import styled from "styled-components";
 import Contacts from "../Components/Contacts"
+import Welcome from "../Components/Welcome";
 
 function Chat() {
 
@@ -37,10 +38,38 @@ function Chat() {
   }, [currentUser])
 
   return (
-    <div className="container">
-          <Contacts contacts={contacts}  />
-    </div>
-  )
+    <>
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts}  />
+   
+          <Welcome />
+        
+      </div>
+    </Container>
+  </>
+);
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat

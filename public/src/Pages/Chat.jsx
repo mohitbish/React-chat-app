341df735dca7557a93b1de1484b634e3f3d7daea
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-14 02:00:43 +1000

    socket installed on server and public

--- public/src/Pages/Chat.jsx
+++ public/src/Pages/Chat.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import {allUsersRoute} from "../Utils/APIRoutes";
 import styled from "styled-components";

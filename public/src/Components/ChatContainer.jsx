a814d9fb18a726a475ed63b2ed6b8be4f8ea8555
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-14 00:54:10 +1000

    emoji picker and css fixed// working

--- public/src/Components/ChatContainer.jsx
+++ public/src/Components/ChatContainer.jsx
@@ -4,7 +4,7 @@ import ChatInput from "./ChatInput";
 import Logout from "./Logout";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-import Messages from "./Messages";
+
 
 
 export default function ChatContainer({ currentChat }) {
@@ -30,15 +30,22 @@ export default function ChatContainer({ currentChat }) {
           </div>
           <Logout />
         </div>
-      <Messages/>
+      <div className="chat-messages">
+        
+      </div>
       <ChatInput handleSendMsg={handleSendMsg}/>
     </Container>
 );
 }
 
 const Container = styled.div`
-  padding-top: 1 rem;
-  overflow: hidden;
+    display: grid;
+    grid-template-rows: 10% 80% 10%;
+    gap: 0.1rem;
+    overflow: hidden;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-rows: 15% 70% 15%;
+    }
 
   .chat-header {
     display: flex;
@@ -58,6 +65,48 @@ const Container = styled.div`
         h3 {
           color: white;
         }
+      }
+    }
+  }
+  .chat-messages {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .message {
+      display: flex;
+      align-items: center;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+          max-width: 70%;
+        }
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
       }
     }
   }
--- public/src/Components/ChatInput.jsx
+++ public/src/Components/ChatInput.jsx
@@ -4,44 +4,52 @@ import { IoMdSend } from "react-icons/io";
 import styled from "styled-components";
 import Picker from "emoji-picker-react";
 
-function ChatInput() {
-    const [msg, setMsg] = useState("");
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-    const handleEmojiPickerhideShow = () => {
-        setShowEmojiPicker(!showEmojiPicker);
-    };
+export default function ChatInput({ handleSendMsg }) {
+  const [msg, setMsg] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const handleEmojiPickerhideShow = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
 
-    const handleEmojiClick = (event, emojiObject) => {
-        let message = msg;
-        message += emojiObject.emoji;
-        setMsg(message);
-    };
+  const handleEmojiClick = (event, emojiObject) => {
+    let message = msg;
+    message += emojiObject.emoji;
+    setMsg(message);
+  };
 
-    
+  const sendChat = (event) => {
+    event.preventDefault();
+    if (msg.length > 0) {
+      handleSendMsg(msg);
+      setMsg("");
+    }
+  };
 
   return (
     <Container>
-        <div className="button-container">
-            <div className="emoji">
-                <BsEmojiSmileFill onClick={handleEmojiPickerhideShow} />
-                {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
-            </div>
+      <div className="button-container">
+        <div className="emoji">
+          <BsEmojiSmileFill onClick={handleEmojiPickerhideShow} />
+          {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
         </div>
-        <form className="input-container" >
-            <input
-                type="text"
-                placeholder="type your message here"
-            />
-            <button type="submit">
-            <IoMdSend />
-            </button>
-        </form>
+      </div>
+      <form className="input-container" onSubmit={(event) => sendChat(event)}>
+        <input
+          type="text"
+          placeholder="type your message here"
+          onChange={(e) => setMsg(e.target.value)}
+          value={msg}
+        />
+        <button type="submit">
+          <IoMdSend />
+        </button>
+      </form>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
-display: grid;
+  display: grid;
   align-items: center;
   grid-template-columns: 5% 95%;
   background-color: #080420;
@@ -134,5 +142,3 @@ display: grid;
     }
   }
 `;
-
-export default ChatInput

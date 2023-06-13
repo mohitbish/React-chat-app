0457355751a7df7b310a7716a4e844a1367a3f58
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-14 01:48:54 +1000

    chat send and recive functionality implemented

--- public/src/Components/ChatContainer.jsx
+++ public/src/Components/ChatContainer.jsx
@@ -2,26 +2,69 @@ import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import ChatInput from "./ChatInput";
 import Logout from "./Logout";
-import { sendMessageRoute } from "../Utils/APIRoutes";
+import { sendMessageRoute,recieveMessageRoute } from "../Utils/APIRoutes";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 
 
 
-export default function ChatContainer({ currentChat, currentUser }) {
+export default function ChatContainer({ currentChat }) {
+
+  const [messages, setMessages] = useState([]);
+  const scrollRef = useRef();
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await JSON.parse(
+        localStorage.getItem("chat-app-current-user")
+      );
+      const response = await axios.post(recieveMessageRoute, {
+        from: data._id,
+        to: currentChat._id,
+      });
+      setMessages(response.data);
+    })();
+  }, [currentChat])
+
+  useEffect(() => {
+    (async () => {
+      const getCurrentChat = async () => {
+        if (currentChat) {
+          await JSON.parse(
+            localStorage.getItem("chat-app-current-user")
+          )._id;
+        }
+      };
+      getCurrentChat();
+    })();
+  }, [currentChat])
   
 
-  const handleSendMsg = async(msg)=>{
+  const handleSendMsg = async (msg) => {
     const data = await JSON.parse(
       localStorage.getItem("chat-app-current-user")
     );
+    
     await axios.post(sendMessageRoute, {
       from: data._id,
       to: currentChat._id,
       message: msg,
     });
 
-  }
+    const msgs = [...messages];
+    msgs.push({ fromSelf: true, message: msg });
+    setMessages(msgs);
+  };
+
+  useEffect(() => {
+    arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
 
   return (
     <Container>
@@ -40,7 +83,21 @@ export default function ChatContainer({ currentChat, currentUser }) {
           <Logout />
         </div>
       <div className="chat-messages">
-        
+        {messages.map((message) => {
+            return (
+              <div ref={scrollRef} key={uuidv4()}>
+                <div
+                  className={`message ${
+                    message.fromSelf ? "sended" : "recieved"
+                  }`}
+                >
+                  <div className="content ">
+                    <p>{message.message}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </div>
       <ChatInput handleSendMsg={handleSendMsg}/>
     </Container>
--- public/src/Utils/APIRoutes.js
+++ public/src/Utils/APIRoutes.js
@@ -5,3 +5,4 @@ export const setAvatarRoute = `${host}/api/auth/setavatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;

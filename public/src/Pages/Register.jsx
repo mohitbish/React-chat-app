310e946c7fc0c691b09acd388cbb2b3aeba89ec2
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-17 06:00:07 +1000

    logo update

--- public/src/Pages/Register.jsx
+++ public/src/Pages/Register.jsx
@@ -101,7 +101,7 @@ export default function Register() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h1>snappy</h1>
+            <h1>V-CHAT</h1>
           </div>
           <input
             type="text"
